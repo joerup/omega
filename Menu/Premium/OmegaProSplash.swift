34faf751b003bb6f3fbd7447ba3af03561885afb
joerup2004 <70416849+joerup2004@users.noreply.github.com>
2023-07-29 14:21:24 -0400

    2.2.0

--- Menu/Premium/OmegaProSplash.swift
+++ Menu/Premium/OmegaProSplash.swift
@@ -20,6 +20,7 @@ struct OmegaProSplash: View {
     @State private var array: [ProFeatureDisplay] = []
     
     @State private var scale: Double = 0.95
+    @State private var pressedScale: Double = 1.0
     @State private var spotlight: Double = 0.95
     
     var theme1: Theme { settings.previewTheme1 }
@@ -29,38 +30,27 @@ struct OmegaProSplash: View {
     var body: some View {
         
         GeometryReader { geometry in
-            
-            VStack(spacing: 0) {
                 
-                Text("OMEGA PRO")
-                    .font(.system(size: 50, weight: .heavy, design: .rounded))
-                    .lineLimit(0)
-                    .minimumScaleFactor(0.5)
-                    .foregroundColor(Color.white)
-                    .shadow(color: .init(white: 0.05).opacity(0.5), radius: 15)
-                    .padding(.top, verticalSizeClass == .compact ? 10 : 30)
-                
-                if array.count == ProFeatureDisplay.allCases.count {
-                    TabView {
-                        ForEach(array) { display in
-                            page(displayType: display, size: geometry.size)
-                        }
-                    }
-                    .tabViewStyle(.page)
+            if verticalSizeClass == .regular {
+                VStack(spacing: 0) {
+                    title
+                    tabs(size: geometry.size)
+                    purchase(size: geometry.size)
                 }
-                else {
-                    Spacer()
-                }
-                
-                purchaseButtons(size: geometry.size)
-                
+                .frame(width: geometry.size.width)
+                .background(Color.init(white: 0.22).edgesIgnoringSafeArea(.all))
             }
-            .frame(width: geometry.size.width)
-            .background(
-                LinearGradient(colors: [.init(white: 0.3), .init(white: 0.2)], startPoint: .top, endPoint: .bottom)
-                    .overlay(theme1.primaryColor.opacity(0.6))
-                    .edgesIgnoringSafeArea(.all)
-            )
+            else if verticalSizeClass == .compact {
+                HStack(spacing: 0) {
+                    tabs(size: geometry.size)
+                        .padding(.top)
+                        .padding(.leading, 1)
+                        .padding(.bottom, -3)
+                    purchase(size: geometry.size)
+                }
+                .frame(minWidth: geometry.size.width)
+                .background(Color.init(white: 0.22))
+            }
         }
         .ignoresSafeArea(.keyboard)
         .sheet(isPresented: self.$settings.purchaseConfirmation) {
@@ -71,11 +61,33 @@ struct OmegaProSplash: View {
         }
         .onAppear {
             array = ProFeatureDisplay.randomArray(startingWith: Settings.settings.proPopUpType)
-            withAnimation(.easeInOut(duration: 3).repeatForever(autoreverses: true)) {
-                scale = 1.0
+        }
+    }
+    
+    private var title: some View {
+        Text("OMEGA PRO")
+            .font(.system(size: 50, weight: .heavy, design: .rounded))
+            .lineLimit(0)
+            .minimumScaleFactor(0.5)
+            .foregroundColor(Color.white)
+            .shadow(color: .init(white: 0.05).opacity(0.5), radius: 15)
+            .padding(.top, verticalSizeClass == .compact ? 10 : 30)
+    }
+    
+    @ViewBuilder
+    private func tabs(size: CGSize) -> some View {
+        Group {
+            if array.count == ProFeatureDisplay.allCases.count {
+                TabView {
+                    ForEach(array) { display in
+                        page(displayType: display, size: size)
+                    }
+                    .edgesIgnoringSafeArea(.horizontal)
+                }
+                .tabViewStyle(.page)
             }
-            withAnimation(.easeInOut(duration: 4).repeatForever(autoreverses: true)) {
-                spotlight = 1.0
+            else {
+                Spacer()
             }
         }
     }
@@ -97,13 +109,9 @@ struct OmegaProSplash: View {
             
             Spacer(minLength: 0)
             
-            VStack {
-                if verticalSizeClass == .regular {
-                    displayType.previews(theme1: theme1, theme2: theme2, theme3: theme3)
-                }
-            }
-            .frame(maxWidth: size.height > 800 ? .infinity : 500)
-            .shadow(radius: 10)
+            displayType.previews(theme1: theme1, theme2: theme2, theme3: theme3)
+                .frame(maxWidth: size.height > 800 ? .infinity : 500)
+                .shadow(radius: 10)
             
             Spacer(minLength: 0)
             
@@ -114,7 +122,7 @@ struct OmegaProSplash: View {
                 .minimumScaleFactor(0.5)
                 .multilineTextAlignment(.center)
                 .shadow(radius: 15)
-                .frame(maxWidth: 500)
+                .frame(maxWidth: min(500, verticalSizeClass == .compact ? size.width*0.4 : size.width))
                 .padding(.vertical, 5)
                 .padding(.horizontal)
                 .padding(.bottom, 40)
@@ -182,12 +190,18 @@ struct OmegaProSplash: View {
         }
     }
     
-    private func purchaseButtons(size: CGSize) -> some View {
+    private func purchase(size: CGSize) -> some View {
         VStack {
+            
+            if verticalSizeClass == .compact {
+                title
+                Spacer(minLength: 0)
+            }
             
             if settings.pro {
                 
                 Button(action: {
+                    pressScale()
                     self.dismiss()
                 }) {
                     HStack {
@@ -214,12 +228,16 @@ struct OmegaProSplash: View {
                 .padding(.horizontal, 20)
                 .padding(.bottom, 20)
                 .padding(.top, 20)
-                .scaleEffect(scale*1.05)
+                .scaleEffect(scale*pressedScale*1.05)
                 .buttonStyle(.plain)
+                .onAppear {
+                    introScale()
+                }
                 
             } else if let product = storeManager.myProducts.first(where: { $0.productIdentifier == "com.rupertusapps.OmegaCalc.PRO" }) {
                 
                 Button(action: {
+                    pressScale()
                     storeManager.purchaseProduct(product: product)
                     print("Buying \(product.localizedTitle)")
                 }) {
@@ -247,8 +265,11 @@ struct OmegaProSplash: View {
                 .padding(.horizontal, 20)
                 .padding(.bottom, 20)
                 .padding(.top, 20)
-                .scaleEffect(scale*1.05)
+                .scaleEffect(scale*pressedScale*1.05)
                 .buttonStyle(.plain)
+                .onAppear {
+                    introScale()
+                }
                 
             } else {
                 
@@ -269,7 +290,7 @@ struct OmegaProSplash: View {
                 .padding(.horizontal, 20)
                 .padding(.bottom, 20)
                 .padding(.top, 20)
-                .scaleEffect(scale*1.05)
+                .scaleEffect(scale*pressedScale*1.05)
             }
             
             Button(action: {
@@ -277,7 +298,7 @@ struct OmegaProSplash: View {
             }) {
                 Text("Restore Purchases")
                     .font(Font.system(.subheadline, design: .rounded).weight(.bold))
-                    .foregroundColor(.black.opacity(0.5))
+                    .foregroundColor(.white)
                     .dynamicTypeSize(..<DynamicTypeSize.xxLarge)
                     .shadow(color: .init(white: 0.3), radius: 20)
             }
@@ -289,14 +310,32 @@ struct OmegaProSplash: View {
             }) {
                 Text("Not Now")
                     .font(Font.system(.subheadline, design: .rounded).weight(.bold))
-                    .foregroundColor(.black.opacity(0.5))
+                    .foregroundColor(.white)
                     .dynamicTypeSize(..<DynamicTypeSize.xxLarge)
                     .shadow(color: .init(white: 0.3), radius: 20)
             }
             .padding(.bottom, 15)
         }
-        .frame(width: size.width)
-        .background(Color.white.opacity(0.3).frame(maxWidth: .infinity).cornerRadius(10).edgesIgnoringSafeArea(.all))
+        .frame(width: verticalSizeClass == .compact ? size.width/2 : size.width)
+        .background(Color.white.opacity(0.2).frame(maxWidth: .infinity).cornerRadius(10).edgesIgnoringSafeArea(.vertical))
+    }
+    
+    private func introScale() {
+        withAnimation(.easeInOut(duration: 3).repeatForever(autoreverses: true)) {
+            scale = 1.0
+        }
+        withAnimation(.easeInOut(duration: 4).repeatForever(autoreverses: true)) {
+            spotlight = 1.0
+        }
+    }
+    private func pressScale() {
+        SoundManager.play(haptic: .heavy)
+        withAnimation(.easeOut(duration: 0.1)) {
+            pressedScale = 0.8
+        }
+        withAnimation(.easeIn(duration: 1.0).delay(0.1)) {
+            pressedScale = 1.0
+        }
     }
 }
 
